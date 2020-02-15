c28fea2a2c76ea984746155f3e273a2db77a4382
Double0h <39421194+Double0h@users.noreply.github.com>
2020-02-15 14:46:21 +0300

    Add files via upload

---
+++ Ceng443/Hw1/source/Simulator.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class Simulator {
+
+    public static void main(String[] args) {
+        SessionParser parser = new SessionParser();
+        Session s = parser.parse("spa.xml");
+        s.simulate();
+    }
+}
